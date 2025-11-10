3ef8a6468528984a6a72e9b8b5c636dfb14dd2a4
mellolo <21921066@zju.edu.cn>
2025-11-10 20:10:38 +0800

    manual strategy

--- http_service/app.py
+++ http_service/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+#app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/api/example', methods=['GET'])
